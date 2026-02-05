1bdd9c5afce82333f51a23c69f9e250f11315386
javier25arroyo <javier25arojas@gmail.com>
2026-02-05 13:19:08 -0600

    Initial commit

---
+++ src/main/java/cr/go/ice/sismo_platform/SismoPlatformApplication.java
@@ -0,0 +1,13 @@
+package cr.go.ice.sismo_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SismoPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SismoPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cr/go/ice/sismo_platform/SismoPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package cr.go.ice.sismo_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SismoPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
